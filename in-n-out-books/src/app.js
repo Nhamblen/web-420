96d65d83328cf8b27aeca62ed165f73b6ed451d4
Noah <nhamblen21@gmail.com>
2025-07-25 18:42:51 -0500

    Add port

--- in-n-out-books/src/app.js
+++ in-n-out-books/src/app.js
@@ -321,5 +321,10 @@ app.use((err, req, res, next) => {
   });
 });
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 // Export the app
 module.exports = app;
